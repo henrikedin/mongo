b4ca4ef8ba2a6034710b7d2db0ca6e2900397b74
Henrik Edin <henrik.edin@mongodb.com>
2019-03-25 16:15:47 -0400

    SERVER-39728 Deinit SSL Manager on Windows in unittests

--- src/mongo/util/net/ssl_manager_windows.cpp
+++ src/mongo/util/net/ssl_manager_windows.cpp
@@ -338,18 +338,30 @@ private:
      */
     CAEngine _serverEngine;
 
+private:
     UniqueCertificate _sslCertificate;
     UniqueCertificate _sslClusterCertificate;
 };
 
-MONGO_INITIALIZER_WITH_PREREQUISITES(SSLManager, ("EndStartupOptionHandling"))
-(InitializerContext*) {
-    if (!isSSLServer || (sslGlobalParams.sslMode.load() != SSLParams::SSLMode_disabled)) {
-        theSSLManager = new SSLManagerWindows(sslGlobalParams, isSSLServer);
-    }
-
-    return Status::OK();
-}
+GlobalInitializerRegisterer sslManagerInitializer(
+    "SSLManager",
+    {"EndStartupOptionHandling"},
+    {},
+    [](InitializerContext*) {
+        if (!isSSLServer || (sslGlobalParams.sslMode.load() != SSLParams::SSLMode_disabled)) {
+            theSSLManager = new SSLManagerWindows(sslGlobalParams, isSSLServer);
+        }
+        return Status::OK();
+    },
+    [](DeinitializerContext* context) {
+		if (theSSLManager)
+		{
+            delete theSSLManager;
+            theSSLManager = nullptr;
+		}
+        
+        return Status::OK();
+    });
 
 SSLConnectionWindows::SSLConnectionWindows(SCHANNEL_CRED* cred,
                                            Socket* sock,
