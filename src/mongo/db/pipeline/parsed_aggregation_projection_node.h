217765870a4ff5c124596206115f6e7f3c8a14ab
Henrik Edin <henrik.edin@mongodb.com>
2019-09-30 13:16:15 -0400

    SERVER-43446 Fix TODO and use StringMap as the required feature is now supported.

--- src/mongo/db/pipeline/parsed_aggregation_projection_node.h
+++ src/mongo/db/pipeline/parsed_aggregation_projection_node.h
@@ -147,8 +147,7 @@ protected:
     // Writes the given value to the output doc, replacing the existing value of 'field' if present.
     virtual void outputProjectedField(StringData field, Value val, MutableDocument* outDoc) const;
 
-    // TODO use StringMap once SERVER-23700 is resolved.
-    stdx::unordered_map<std::string, std::unique_ptr<ProjectionNode>> _children;
+    StringMap<std::unique_ptr<ProjectionNode>> _children;
     stdx::unordered_map<size_t, std::unique_ptr<ProjectionNode>> _arrayBranches;
 
     StringMap<boost::intrusive_ptr<Expression>> _expressions;
