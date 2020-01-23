493d41ff7a2339fab0676f73c0adaffc567a3a89
Gabriel Russell <gabriel.russell@mongodb.com>
2020-01-23 15:21:56 -0500

    more batchers stuff

--- batcher.pl
+++ batcher.pl
@@ -57,4 +57,5 @@ for my $batch (sort keys %$found_batches) {
     run(qw(git cifa));
     run("./logv1tologv2",@files); 
     run(qw(python ~/git/kernel-tools/codereview/upload.py --git_no_find_copies -y),"-r", $batch_reviewers->{$batch}, "-m", "structured logging auto-conversion of ".$batch, "HEAD");
+    run(qw(evergreen patch -p mongodb-mongo-master  --yes), "-d", "structured logging auto-conversion of $batch");
 }
