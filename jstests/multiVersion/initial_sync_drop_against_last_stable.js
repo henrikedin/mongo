8d2039276a6a5bbceae7dc2414f965d3f198df57
Henrik Edin <henrik.edin@mongodb.com>
2020-02-18 13:28:35 -0500

    fix initial_sync_drop_against_last_stable.js

--- jstests/multiVersion/initial_sync_drop_against_last_stable.js
+++ jstests/multiVersion/initial_sync_drop_against_last_stable.js
@@ -78,7 +78,7 @@ function setupTest({failPoint, extraFailPointData, secondaryStartupParams}) {
     replTest.waitForState(secondary, ReplSetTest.State.STARTUP_2);
 }
 
-function finishTest({failPoint, expectedLog, waitForDrop, createNew}) {
+    function finishTest({ failPoint, expectedLog, expectedLogId, waitForDrop, createNew}) {
     // Get the uuid for use in checking the log line.
     let uuid = getUUIDFromListCollections(primaryDB, collName);
 
@@ -99,15 +99,25 @@ function finishTest({failPoint, expectedLog, waitForDrop, createNew}) {
     jsTestLog("Allowing secondary to continue.");
     assert.commandWorked(secondary.adminCommand({configureFailPoint: failPoint, mode: 'off'}));
 
-    if (expectedLog) {
-        expectedLog = eval(expectedLog);
-        if (isJsonLog(primaryColl.getMongo())) {
-            expectedLog = expectedLog.replace(/"/g, "\\\"");
-        }
-        jsTestLog(expectedLog);
-        checkLog.contains(secondary, expectedLog);
-    }
+    if (isJsonLog(primaryColl.getMongo())) {
+        if (expectedLogId) {
+            let attrValues = {
+                "ns": nss, "uuid": function (attr) {
+                    return BinData(parseInt(attr.uuid.$binary.subType), attr.uuid.$binary.base64).toString() === uuid.toString();
+                }
+            }
 
+            checkLog.containsJson(secondary, expectedLogId, attrValues);
+        }
+    }
+    else {
+        if (expectedLog) {
+            expectedLog = eval(expectedLog);
+            jsTestLog(expectedLog);
+            checkLog.contains(secondary, expectedLog);
+        }
+    }
+    
     jsTestLog("Waiting for initial sync to complete.");
     replTest.waitForState(secondary, ReplSetTest.State.SECONDARY);
 
@@ -148,6 +158,7 @@ runDropTest({
     failPoint: "initialSyncHangCollectionClonerAfterHandlingBatchResponse",
     secondaryStartupParams: {collectionClonerBatchSize: 1},
     waitForDrop: true,
+    expectedLogId: 21132,
     expectedLog:
         "`CollectionCloner ns: '${nss}' uuid: ${uuid} stopped because collection was dropped on source.`"
 });
@@ -158,6 +169,7 @@ runDropTest({
     failPoint: "initialSyncHangCollectionClonerAfterHandlingBatchResponse",
     secondaryStartupParams: {collectionClonerBatchSize: 1},
     waitForDrop: true,
+    expectedLogId: 21132,
     expectedLog:
         "`CollectionCloner ns: '${nss}' uuid: ${uuid} stopped because collection was dropped on source.`",
     createNew: true
