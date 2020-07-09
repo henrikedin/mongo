1ae2c05bb9f9b0edb879899f8bf291035bcdf8d9
Henrik Edin <henrik.edin@mongodb.com>
2020-07-09 14:22:27 -0400

    disable test

--- jstests/concurrency/fsm_workloads/agg_group_external.js
+++ jstests/concurrency/fsm_workloads/agg_group_external.js
@@ -7,6 +7,7 @@
  *
  * The data passed to the $group is greater than 100MB, which should force
  * disk to be used.
+ * @tags: [incompatible_with_eft]
  */
 load('jstests/concurrency/fsm_libs/extend_workload.js');  // for extendWorkload
 load('jstests/concurrency/fsm_workloads/agg_base.js');    // for $config
