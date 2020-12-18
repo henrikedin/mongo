305b9ec790fd4e0cf13883bf5fe751f14477a9db
Henrik Edin <henrik.edin@mongodb.com>
2020-12-18 16:57:24 -0500

    make repl stuff untimestamped

--- src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
+++ src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
@@ -360,17 +360,9 @@ void ReplicationCoordinatorExternalStateImpl::clearAppliedThroughIfCleanShutdown
         loadLastOpTimeAndWallTimeResult.isOK() &&
         loadLastOpTimeAndWallTimeResult.getValue().opTime ==
             _replicationProcess->getConsistencyMarkers()->getAppliedThrough(opCtx)) {
-        // Clear the appliedThrough marker to indicate we are consistent with the top of the
-        // oplog. We record this update at the 'lastAppliedOpTime'. If there are any outstanding
-        // checkpoints being taken, they should only reflect this write if they see all writes up
-        // to our 'lastAppliedOpTime'.
-        auto lastAppliedOpTime = repl::ReplicationCoordinator::get(opCtx)->getMyLastAppliedOpTime();
 
         invariant(opCtx->lockState()->isRSTLExclusive());
-        // Since we acquired RSTL in mode X, there can't be any active readers. So, it's safe to
-        // write the minvalid document to the storage.
-        _replicationProcess->getConsistencyMarkers()->clearAppliedThrough(
-            opCtx, lastAppliedOpTime.getTimestamp());
+        _replicationProcess->getConsistencyMarkers()->clearAppliedThrough(opCtx, Timestamp());
     }
 }
 
@@ -505,9 +497,7 @@ OpTime ReplicationCoordinatorExternalStateImpl::onTransitionToPrimary(OperationC
     // We record this update at the 'lastAppliedOpTime'. If there are any outstanding
     // checkpoints being taken, they should only reflect this write if they see all writes up
     // to our 'lastAppliedOpTime'.
-    auto lastAppliedOpTime = repl::ReplicationCoordinator::get(opCtx)->getMyLastAppliedOpTime();
-    _replicationProcess->getConsistencyMarkers()->clearAppliedThrough(
-        opCtx, lastAppliedOpTime.getTimestamp());
+    _replicationProcess->getConsistencyMarkers()->clearAppliedThrough(opCtx, Timestamp());
 
     writeConflictRetry(opCtx, "logging transition to primary to oplog", "local.oplog.rs", [&] {
         AutoGetOplog oplogWrite(opCtx, OplogAccessMode::kWrite);
