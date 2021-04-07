af2bf0eb57a006e5d42533f24aba4cc856a7a5f0
Henrik Edin <henrik.edin@mongodb.com>
2021-04-07 16:03:12 -0400

    Revert "Revert "use resize instead""
    This reverts commit b9027b9f2ea1b0ff7226ca06e4ec74bf62717be2.

--- src/mongo/db/timeseries/bucket_catalog.cpp
+++ src/mongo/db/timeseries/bucket_catalog.cpp
@@ -97,28 +97,29 @@ void normalizeObject(BSONObjBuilder* builder, const BSONObj& obj) {
     }
     // Put all elements in a buffer, sort it and then continue normalize in sorted order
     boost::container::small_vector<Field, 16> fields;
-    fields.reserve(num);
+    fields.resize(num);
+    /*fields.reserve(num);
     std::transform(obj.begin(), obj.end(), std::back_inserter(fields), [](const BSONElement& elem) {
         return Field{elem.fieldNameStringData(), elem.size()};
     });
     std::sort(fields.begin(), fields.end());
     for (auto&& field : fields) {
         normalizeElement(field.element());
-    }
+    }*/
 
-    /*auto fields = std::make_unique<Field[]>(num);
+    /*auto fields = std::make_unique<Field[]>(num);*/
     BSONObjIterator bsonIt(obj);
     int i = 0;
     while (bsonIt.more()) {
         auto elem = bsonIt.next();
         fields[i++] = {elem.fieldNameStringData(), elem.size()};
     }
-    auto it = fields.get();
-    auto end = fields.get() + num;
+    auto it = fields.begin();
+    auto end = fields.end();
     std::sort(it, end);
     for (; it != end; ++it) {
         normalizeElement(it->element());
-    }*/
+    }
 }
 
 UUID getLsid(OperationContext* opCtx, BucketCatalog::CombineWithInsertsFromOtherClients combine) {
